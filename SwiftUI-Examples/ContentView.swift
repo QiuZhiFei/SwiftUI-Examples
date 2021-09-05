355c5caa2306fb499ceac72416343f9681814ca5
qiuzhifei <qiuzhifei521@gmail.com>
2021-09-05 14:15:27 +0800

    Initial Commit

---
+++ SwiftUI-Examples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Examples
+//
+//  Created by zhifei qiu on 2021/9/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
