e81abbe24955904f5a288dde834c1d8193246ff3
qiuzhifei <qiuzhifei521@gmail.com>
2021-09-25 12:31:47 +0800

    f

--- SwiftUI-Examples/ContentView.swift
+++ SwiftUI-Examples/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    VStack(spacing: 11.0) {
+      Text("title")
+        .font(.title)
+      HStack {
+        Text("description")
+          .font(.headline)
+        Spacer()
+        Text("description")
+          .font(.headline)
+      }
+      Image("idol_img_1")
+        .clipShape(Circle())
+        .overlay(Circle().stroke(Color.gray, lineWidth: 4))
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
